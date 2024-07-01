cdbc60661edf1f8f727f703e3f422e20f6f0917f
yadelagadou <david.vallet22@gmail.com>
2024-07-01 09:48:14 +0200

    Add files via upload

--- script3.js
+++ script3.js
@@ -179,3 +179,15 @@ function enterFullScreen(element) {
         element.msRequestFullscreen();
     }
 }
+
+function exitFullScreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+        document.msExitFullscreen();
+    }
+}
